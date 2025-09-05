390998c422aed00c160182b4e9ca9ee3b8043748
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-09-05 15:31:13 +0000

    [feat] .

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+// import sqlite3 from 'sqlite3'; import { open } from 'sqlite'; import { dbPath } from '../src/utils/dbpath.js'; export const connectDB = async () => { try { const db = await open({ filename: dbPath, driver: sqlite3.Database, }); console.log("Banco de dados conectado com sucesso!"); return db; } catch (error) { console.error("Erro ao conectar ao banco de dados:", error); throw error; } };
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import { dbPath } from '../src/utils/dbpath.js';
@@ -8,6 +9,7 @@ export const connectDB = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
+
     console.log("Banco de dados conectado com sucesso!");
     return db;
   } catch (error) {
